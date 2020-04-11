3e826d7f245585e764ba1e70a912578b186532e7
Vivid <vividbloop@gmail.com>
2020-04-11 18:13:52 -0400

    explore query

--- scratch.py
+++ scratch.py
@@ -1,9 +1,15 @@
+import discogs_client
+
 # Understand discogs api
 
 # Create discogs connection
+discogs = discogs_client.Client('SpinYoRecords')
 
 # Send a query
+results = discogs.search('Sunny Came Home',
+                         type='release')
 
+results[0].artists[0]
 # Select a random result
 
 # Select the link
