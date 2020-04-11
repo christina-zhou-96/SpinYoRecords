471dbbd1d10b8688dc27a67db0bb5aeb0b9c2ad1
Vivid <vividbloop@gmail.com>
2020-04-11 17:53:40 -0400

    skeleton local app

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
+from flask import Flask,redirect,request
+
 # Create app instance
+app = Flask(__name__)
 
 # Create homepage with button
+@app.route("/")
+def home():
+    return """
+    <html><body>
+        <form action="/query">
+            <input type='submit' value='I'm Feeling Lucky'>
+    </body></html>
+    """
 
 # Backend query to discogs
+@app.route("/query")
+def link():
+    # Scratch implementation
+    return redirect("https://www.google.com/")
 
 # Run app
+if __name__ == "__main__":
+    app.run(debug=True)
